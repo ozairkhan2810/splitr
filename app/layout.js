7419b4bd499e455b7d7493829caeb0eae6448720
ozairkhan2810 <ozairkhan2810@gmail.com>
2025-05-11 11:43:42 +0530

    Storing in convex database

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import { ConvexClientProvider } from "@/components/convex-client-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,26 @@
+"use client"
+
+import { useStoreUser } from '@/hooks/use-store-user'
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
+import { BarLoader } from 'react-spinners'
 
 const Header = () => {
+  const {isLoading} = useStoreUser()
+
   return (
-    <div>Header</div>
+    <header className='fixed top-0 w-full border-b bg-white/95 backdrop-blur z-50 supports[backdrop-filter]:bg-white/60'>
+      <nav>
+        <SignedOut>
+          <SignInButton/>
+          <SignUpButton/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
+      </nav>
+    {isLoading && <BarLoader width={"100%"} color='#36d7b7' />}
+    </header>
   )
 }
 
